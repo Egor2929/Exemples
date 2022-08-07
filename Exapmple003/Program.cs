dc557ddde41fd0e221a1ee351f06d0f546cf600a
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-07 15:34:40 +0300

    Exaple03003 add random

--- Exapmple003/Program.cs
+++ Exapmple003/Program.cs
@@ -1,5 +1,7 @@
-﻿int namberA = 7;
-int namberB = 4;
-int result = namberA + namberB;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,20);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.Write (result);
 
