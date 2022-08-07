bda83c1e4926126a520c371f1ce97549ce7255fd
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-07 16:09:59 +0300

    Exaple006

---
+++ Example006/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 5;
+int b = 20;
+int c = 57;
+int d = 75;
+int f = 3;
+int e = 777;
+
+int max = a;
+
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+if (e > max) max = e;
+
+Console.Write("max= :");
+Console.WriteLine(max);
