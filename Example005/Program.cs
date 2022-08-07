5af927fcd1e17c79ecbf4e9d4908415c1c2cc124
Egor Еtrofimov <martynenkoa01@gmail.com>
2022-08-07 15:46:19 +0300

    Exaple005

---
+++ Example005/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура,это Маша!");
+}
+else 
+{
+    Console.WriteLine("ПРивет, ");
+    Console.WriteLine(username);
+}
+
+
